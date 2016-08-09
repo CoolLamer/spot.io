ad84f19d588814e59507ecc76f2f7868797d29f4
Lukáš Bauer <bauer.lukas@centrum.cz>
2016-08-10 00:35:44 +0200

    Remember Messages

--- app/socket.js
+++ app/socket.js
@@ -1,11 +1,17 @@
 function socket(http){
     var io = require('socket.io')(http);
     require('./onlineUsers.js')(io);
+    var messages = [];
 
     io.on('connection', function(socket){
+        messages.forEach(function(data){
+           socket.emit('message', data);
+        });
+
         socket.on('click', function(data){
             console.log('send message');
             io.emit('message', data);
+            messages.push(data);
         })
     });
     return io;
